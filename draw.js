49c715bdaa35009c7a955458d6aff0fa2f7e9ebb
Kinvert <keith@kinvert.com>
2025-07-15 02:22:09 -0400

    New JS File for Draw

---
+++ draw.js
@@ -0,0 +1,120 @@
+function draw_car(px, py, ang) {
+    // Placeholder Car
+    ctx.strokeStyle = 'magenta';
+    ctx.lineWidth = 8;
+    ctx.beginPath();
+    ctx.moveTo(px-10*Math.cos(ang), py-10*Math.sin(ang));
+    ctx.lineTo(px+10*Math.cos(ang), py+10*Math.sin(ang));
+    ctx.stroke();
+}
+
+function draw_drive(px_history, py_history) {
+    ctx.strokeStyle = 'magenta';
+    ctx.lineWidth = 1;
+    ctx.moveTo(px_history[0], py_history[0]);
+    for (var i = 1; i < px_history.length; i++) {
+        if (Math.abs(px_history[i]-px_history[i-1]) > 10 || Math.abs(py_history[i]-py_history[i-1]) > 10) {
+            ctx.stroke();
+            ctx.moveTo(px_history[i], py_history[i]);
+        }
+        ctx.lineTo(px_history[i], py_history[i]);
+    }
+    ctx.stroke();
+}
+
+function draw() {
+    
+    ctx.drawImage(track, 0, 0);
+    //draw_fov();
+    //draw_tiny_top_view();
+
+    if (ISOMETRIC) {
+        draw_mode_7();
+    }
+
+    // TODO Difference between this img and last img to only show changed pixels
+}
+
+function draw_top_view() {
+    /* TINY TOP VIEW */
+    /*
+    ctx2.save()
+    ctx2.rotate(-Math.PI/2-ang);
+    ctx2.drawImage(canv, -flx, -fly);
+    ctx2.restore();
+    */
+}
+
+function draw_mode_7() {
+    var cos_ang = Math.cos(ang);
+    var sin_ang = Math.sin(ang);
+
+    var perspective = ctx2.createImageData(W, H);
+    var topDown = ctx.getImageData(0, 0, W, H);
+    // Mode 7 like algorithm
+    // http://coranac.com/tonc/text/mode7.htm
+    for (var i = 4*W*(horiz); i < topDown.data.length; i += 4) {
+        var imgy = Math.floor(i/(4*W)); // Image Plane Y Coordinate. 4 bytes per pixel. w pixels wide. Increment Y
+        var imgx = Math.floor((i/4)%W)-(W/2); // Imag Plane X Coord. 4 bytes/pix. w pix wide. 0 at center not at left
+        var sf = imgy/height;                  // sf is a scaling factor making near things large
+        var z = Math.floor(5000/(imgy-horiz)); // Depth Map
+        var z2 = Math.floor( 0.75*(W/2)*height / (imgy-horiz) ); // Depth Map attempt 2
+        var view_angley = imgy-(horiz);         // y view angle this pass
+        var view_anglex = view_angley*(W/H); // x view angle this pass
+
+        var xval = (imgx/(sf*view_anglex))*(W/4);
+        var yval = (height/view_angley)*(horiz);
+
+        var xval2 = imgx/(z);
+        var yval2 = imgy/z;
+
+        // z2 and yval are nearly identical
+        var xprime = Math.floor(px - (xval * sin_ang) + (z2 * cos_ang)); // top down x coord of pixel
+        var yprime = Math.floor(py + (xval * cos_ang) + (z2 * sin_ang)); // top down y coord of pixel
+
+        if (i % 100000 == 0 && log_vars == 1) {
+            console.log(' ');
+            console.log(' ');
+            console.log('i', i, 'z', z, 'z2', z2);
+            console.log('ang', ang, 'cos_ang', cos_ang, 'sin_ang', sin_ang);
+            console.log('imgx', imgx, 'imgy', imgy, 'sf', sf, 'viewx', view_anglex, 'viewy', view_angley);
+            console.log('xval2', xval2, 'yval2', yval2);
+            console.log('xval', xval, 'yval', yval, 'xp', xprime, ' yp', yprime);
+        }
+        
+        if(xprime >= 0 && xprime <= W && yprime >= 0 && yprime <= H){
+            var idx = ((yprime * W) + xprime) * 4;
+            perspective.data[i] = topDown.data[idx];
+            perspective.data[i+1] = topDown.data[idx+1];
+            perspective.data[i+2] = topDown.data[idx+2];
+            perspective.data[i+3] = topDown.data[idx+3];
+        }
+    }
+    log_vars = 0;
+    ctx2.putImageData(perspective, 0, 0);
+}
+
+function draw_fov() {
+    // Draw Field of View
+    var flx = px + Math.cos(ang - fovh) * far; // Far Left X
+    var fly = py + Math.sin(ang - fovh) * far;
+    ctx.beginPath();
+    ctx.arc(flx, fly, 5, 0, 2*Math.PI, false);
+    ctx.fillStyle = 'magenta';
+    ctx.fill();
+    var frx = px + Math.cos(ang + fovh) * far; // Far Right X
+    var fry = py + Math.sin(ang + fovh) * far;
+    ctx.beginPath();
+    ctx.arc(frx, fry, 5, 0, 2*Math.PI, false);
+    ctx.fill();
+    var nlx = px + Math.cos(ang + fovh) * near; // Near Left X
+    var nly = py + Math.sin(ang + fovh) * near;
+    ctx.beginPath();
+    ctx.arc(nlx, nly, 5, 0, 2*Math.PI, false);
+    ctx.fill();
+    var nrx = px + Math.cos(ang - fovh) * near; // Near Right X
+    var nry = py + Math.sin(ang - fovh) * near;
+    ctx.beginPath();
+    ctx.arc(nrx, nry, 5, 0, 2*Math.PI, false);
+    ctx.fill();
+}
